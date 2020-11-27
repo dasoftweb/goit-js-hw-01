8f2129d4f65972215060de15ff0676e945988bb3
Klirik <dasoftweb@gmail.com>
2020-11-27 18:41:07 +0200

    Update

--- js/task-03.js
+++ js/task-03.js
@@ -4,10 +4,8 @@ let message = "Доступ запрещен, неверный пароль!";
 
 if (password === null) {
   message = "Отменено пользователем!";
-  alert(message);
 } else if (password === ADMIN_PASSWORD) {
   message = "Добро пожаловать!";
-  alert(message);
-} else {
-  alert(message);
 }
+
+alert(message);
